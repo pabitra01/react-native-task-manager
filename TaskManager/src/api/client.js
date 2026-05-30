289b5fee3180d13df9ee36c8dee43e7b87fd8220
Pabitra Ranjan Rout <pabi@Pabitras-Mac-mini.local>
2026-05-30 11:38:09 +0530

    Read API_URL from .env using react-native-config

--- TaskManager/src/api/client.js
+++ TaskManager/src/api/client.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
 import { Platform } from 'react-native';
+import Config from 'react-native-config';
 import { getToken, removeToken } from '../utils/storage';
 
-const baseURL = Platform.select({
-  android: 'http://10.0.2.2:5000/api',
-  ios: 'http://localhost:5000/api',
-  default: 'http://localhost:5000/api',
+const baseURL = Config.API_URL || Platform.select({
+  android: 'http://10.0.2.2:5001/api',
+  ios: 'http://localhost:5001/api',
+  default: 'http://localhost:5001/api',
 });
 
 const client = axios.create({
@@ -18,22 +19,22 @@ const client = axios.create({
 
 // Request interceptor - attach Authorization header
 client.interceptors.request.use(
-  async config => {
+  async (config) => {
     const token = await getToken();
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
   },
-  error => {
+  (error) => {
     return Promise.reject(error);
   },
 );
 
 // Response interceptor - handle 401
 client.interceptors.response.use(
-  response => response,
-  async error => {
+  (response) => response,
+  async (error) => {
     if (error.response && error.response.status === 401) {
       await removeToken();
     }
